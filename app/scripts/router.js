abe2ce16a6d5469c40de32ff053a8bc92d2b06ac
JAMES LEE <jamesdlee6203@gmail.com>
2016-07-22 13:05:41 -0500

    started working on edit truck page

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,25 +2,32 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import nav from './views/nav';
 import trucksFeed from './views/truckFeedView';
-import store from "./store";
+import store from './store';
+import loginView from './views/loginView';
+import addTruck from './views/addTruckView';
 
 const Router = Backbone.Router.extend({
   routes: {
     login: 'loginFunction',
     truckfeed: 'truckFeedFunction',
     addtruck: 'addTruckFunction',
+    edittruck: 'editTruckFunction',
     logout: 'logoutFunction',
     '/*': 'truckFeedFunction'
     //catch all to redirect user to home page
   },
   loginFunction: function(){
-  $('.container').empty().append(nav.render().$el);
+  $('.container').empty().append(nav.render().$el).append(loginView.render().$el);
+
 },
   truckFeedFunction: function(){
     $('.container').empty().append(nav.render().$el).append(trucksFeed.render().$el);
   },
   addTruckFunction: function(){
-    $('.container').empty().append(nav.render().$el);
+    $('.container').empty().append(nav.render().$el).append(addTruck.render().$el);
+  },
+  editTruckFunction: function(){
+    $('.container').empty().append(nav.render().$el).append(addTruck.render(true).$el);
   },
   logoutFunction: function(){
     store.session.clear();
--- app/scripts/views/addTruckView.js
+++ app/scripts/views/addTruckView.js
@@ -4,44 +4,53 @@ import trucks from './collections/trucks';
 // Coop worked on this, might need to double check the create function below
 
 const AddTruck = Backbone.View.extend({
-tagName: 'div',
-className: 'newTruck',
-template: function (){
-  return `
-  <input type="text" placeholder="name of truck" value="">
-  <input type="checkbox" name="italian" value="Italian">
-  <input type="checkbox" name="chinese" value="Chinese">
-  <input type="checkbox" name="Thai" value="Thai">
-  <input type="checkbox" name="Greek" value="Greek">
-  <input type="checkbox" name="Mexican" value="Mexican">
-  <input type="checkbox" name="American" value="American">
-  <input type="checkbox" name="Latin" value="Latin">
-  <input type="checkbox" name="Dessert" value="Dessert">
-  <input type="checkbox" name="Vegan" value="Vegan">
-  <input type="checkbox" name="Vegeterian" value="Vegeterian">
-  <input type="checkbox" name="Brunch" value="Brunch">
-  <input type="checkbox" name="Coffee/Tea" value="Coffee/Tea">
-  <input type="checkbox" name="Cuban" value="Cuban">
-  <input type="checkbox" name="Vietnamese" value="Vietnamese">
-  <input type="checkbox" name="Gluten Free" value="Gluten Free">
-  <input type="text" placeholder="Type of Cuisine" value="">
-  <input class name = "submit" type="button" name="submit" value="Submit">
-  `;
-},
-render: function (){
-  this.$el.html(this.template());
+  tagName: 'div',
+  className: 'newTruck',
+  template: function (){
+    var toReturn = `
+    <input type="text" placeholder="name of truck" value="">
+    <input type="checkbox" name="italian" value="Italian">
+    <input type="checkbox" name="chinese" value="Chinese">
+    <input type="checkbox" name="Thai" value="Thai">
+    <input type="checkbox" name="Greek" value="Greek">
+    <input type="checkbox" name="Mexican" value="Mexican">
+    <input type="checkbox" name="American" value="American">
+    <input type="checkbox" name="Latin" value="Latin">
+    <input type="checkbox" name="Dessert" value="Dessert">
+    <input type="checkbox" name="Vegan" value="Vegan">
+    <input type="checkbox" name="Vegeterian" value="Vegeterian">
+    <input type="checkbox" name="Brunch" value="Brunch">
+    <input type="checkbox" name="Coffee/Tea" value="Coffee/Tea">
+    <input type="checkbox" name="Cuban" value="Cuban">
+    <input type="checkbox" name="Vietnamese" value="Vietnamese">
+    <input type="checkbox" name="Gluten Free" value="Gluten Free">
+    <input type="text" placeholder="Type of Cuisine" value="">
+    `;
 
-},
+    if (true){
+      toReturn += '<input class="make-edit" type="button" name="make-edit" value="Submit">';
+    } else {
+      toReturn += '<input class="submit" type="button" name="submit" value="Submit">';
+    }
 
-events: {
-  'click .submit' : 'addTruck'
-},
+    return toReturn;
+  },
+  render: function (editing){
+    this.$el.html(this.template(editing));
 
-addTruck: function (){
-  this.model = trucks.create(addTruck);
-}
+  },
+  events: {
+    'click .submit' : 'addTruck',
+    'click .make-edit' : 'editTruck'
+  },
+  editTruck: function(){
+
+  },
+  addTruck: function (){
+    this.model = trucks.create(addTruck);
+  }
 });
 
-let addTruck = new AddTruck ();
+let addTruck = new AddTruck();
 
 export default addTruck;
