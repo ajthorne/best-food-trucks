847599dfd351a156dea0f2a4ab2a19477096e343
Amanda Thorne <manda.thorne@gmail.com>
2016-07-24 18:53:55 -0500

    merge conflicts

--- app/scripts/models/session.js
+++ app/scripts/models/session.js
@@ -11,19 +11,19 @@ const Session = Backbone.Model.extend({
 
   },
 
-  parse: function(response) {
-    //when data is returned from server, keep track of the things below to pull later
-    if (response) {
-      return {
-        username: response.username,
-        // edit by James, set authtoken, userId, and favorites array for this session
-        authtoken: response.authtoken,
-        // userId: response._id,
-        // favorites: response.favorites
-
-      };
-    }
-  },
+  // parse: function(response) {
+  //   //when data is returned from server, keep track of the things below to pull later
+  //   if (response) {
+  //     return {
+  //       username: response.username,
+  //       // edit by James, set authtoken, userId, and favorites array for this session
+  //       authtoken: response.authtoken,
+  //       // userId: response._id,
+  //       // favorites: response.favorites
+  //
+  //     };
+  //   }
+  // },
 
   // will change when BE-FE connection is established
   login: function(username, password) {
